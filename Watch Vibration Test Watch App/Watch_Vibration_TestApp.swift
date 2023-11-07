b383afbdf1245dda38bc7a8fa0e261f5e176e38b
Dennis <45356478+DennisMoschina@users.noreply.github.com>
2023-11-07 11:49:47 +0100

    Initial Commit

---
+++ Watch Vibration Test Watch App/Watch_Vibration_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Watch_Vibration_TestApp.swift
+//  Watch Vibration Test Watch App
+//
+//  Created by Dennis Moschina on 07.11.23.
+//
+
+import SwiftUI
+
+@main
+struct Watch_Vibration_Test_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Watch Vibration Test/Watch_Vibration_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Watch_Vibration_TestApp.swift
+//  Watch Vibration Test
+//
+//  Created by Dennis Moschina on 07.11.23.
+//
+
+import SwiftUI
+
+@main
+struct Watch_Vibration_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
